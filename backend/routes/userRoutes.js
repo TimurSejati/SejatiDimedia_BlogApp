1e562bd3bd889215b05b68678460a2af57a3e9a5
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2024-01-16 22:23:43 +0700

    [upadte] api login & user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { registerUser } from "../controllers/userControllers.js";
 const routes = express.Router();
+import {
+  loginUser,
+  registerUser,
+  userProfile,
+} from "../controllers/userControllers.js";
+import { authGuard } from "../middleware/authMiddleware.js";
 
 routes.post("/register", registerUser);
+routes.post("/login", loginUser);
+routes.get("/profile", authGuard, userProfile);
 
 export default routes;
